764f0f6576ab3c848484bf73a3baf09170c9c330
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-09-28 18:19:19 +0800

    feat: user can create post

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,20 +2,75 @@ import { useLoaderData } from "react-router";
 import "../styles/HomePage.css";
 import Page from "./Page.jsx";
 import PostCard from "../components/PostCard.jsx";
+import { useEffect, useState } from "react";
+import { useAuth } from "../AuthProvider.jsx";
 
 export default function HomePage() {
+  const { user } = useAuth();
   const data = useLoaderData();
+  const [posts, setPosts] = useState();
 
-  if (!data) {
+  useEffect(() => {
+    setPosts(data.posts);
+  }, []);
+
+  if (!posts) {
     return;
   }
+
+  const handlePostFormSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(event.currentTarget);
+    const content = formData.get("content");
+    const createPost = async () => {
+      try {
+        const endpoint = import.meta.env.VITE_API_URL + "/posts";
+        const res = await fetch(endpoint, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+          body: JSON.stringify({ content }),
+        });
+        const json = await res.json();
+        if (json) {
+          form.reset();
+          setPosts((prev) => [
+            {
+              content,
+              id: prev.length,
+              author: { displayName: user.displayName },
+            },
+            ...prev,
+          ]);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    createPost();
+  };
 
   const handleLikeButtonClick = (event) => {};
 
   return (
     <Page>
       <h2 className="home-heading">Posts</h2>
-      {data.posts.map((post) => (
+      <form onSubmit={handlePostFormSubmit} method="post">
+        <h3>Create Post</h3>
+        <textarea
+          name="content"
+          id="post-content"
+          cols={50}
+          placeholder="Create a post..."
+          required
+        ></textarea>
+        <button className="button" type="submit">
+          Post
+        </button>
+      </form>
+      <h3>Feed</h3>
+      {posts.map((post) => (
         <PostCard
           post={post}
           key={post.id}
