541ec5f47c3983d9ed2c19ecaf9fd8a73083d65f
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-10-02 11:12:09 +0800

    fix: typo on footer copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>© 2025 D Mailer. All rights reserved.</p>
+      <p>© 2025 Spaces. All rights reserved.</p>
     </footer>
   );
 }
