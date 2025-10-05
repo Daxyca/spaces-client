8369ffb69c84fc7a333ee641012c0302c028363f
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-10-05 20:25:18 +0800

    refactor: change logout page logic

--- src/pages/LogoutPage.jsx
+++ src/pages/LogoutPage.jsx
@@ -2,44 +2,36 @@ import "../styles/AuthPage.css";
 import Page from "./Page.jsx";
 import { useAuth } from "../AuthProvider.jsx";
 import { useEffect } from "react";
-import { useNavigate } from "react-router";
 
 export default function LoginPage() {
   const { user, logout } = useAuth();
-  const navigate = useNavigate();
 
   useEffect(() => {
     if (!user) {
-      setTimeout(() => {
-        navigate("/");
-      }, 3000);
       return;
-    } else {
-      const submit = async () => {
-        try {
-          const endpoint = import.meta.env.VITE_API_URL + "/auth/logout";
-          const res = await fetch(endpoint, {
-            method: "DELETE",
-            credentials: "include",
-          });
-          const json = await res.json();
-          if (json.error) {
-            throw new Error(json.error.message);
-          }
-          logout();
-        } catch (err) {
-          console.error(err);
-        }
-      };
-      if (user) {
-        submit();
-      }
     }
-  }, [user]);
+    const submit = async () => {
+      try {
+        const endpoint = import.meta.env.VITE_API_URL + "/auth/logout";
+        const res = await fetch(endpoint, {
+          method: "DELETE",
+          credentials: "include",
+        });
+        const json = await res.json();
+        if (json.error) {
+          throw new Error(json.error.message);
+        }
+        logout();
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    submit();
+  }, []);
 
   return (
     <Page>
-      <p>Logged out successfully!</p>
+      <p>Logging out...</p>
     </Page>
   );
 }
