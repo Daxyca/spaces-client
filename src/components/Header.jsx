7ad385d312513d6e4f41d135f596ae7053be6503
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-10-09 16:31:41 +0800

    refactor: change edit space link label

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -63,8 +63,7 @@ function NavListCenter({ spacesNavlist }) {
           Spaces <DownSymbol />
         </p>
         <Link className="nav-link nav-space-link" to="/spaces">
-          <span className="sr-only">View and </span>
-          (Edit Spaces)
+          <span className="sr-only">View and Edit </span>Spaces ✎
         </Link>
         {spaces.map((space) => (
           <Link
