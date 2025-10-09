e04db77aeb89d87b88650f2b9964b6efbb0a4b61
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-10-09 13:07:19 +0800

    refactor: use local static asset for default avatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,16 +1,7 @@
 export default function Avatar({ picture, alt = "avatar" }) {
   return (
     <div className="avatar-container">
-      <img
-        src={
-          picture
-            ? picture.startsWith("/")
-              ? import.meta.env.VITE_API_BASE_URL + picture
-              : picture
-            : import.meta.env.VITE_DEFAULT_AVATAR_URL
-        }
-        alt={alt}
-      />
+      <img src={picture ? picture : "/default.jpg"} alt={alt} />
     </div>
   );
 }
