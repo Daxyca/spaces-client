ea3178a6a621936a692459b1f295ac5d6dbd6f7c
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-10-10 04:42:36 +0800

    refactor: comment card separate to component

---
+++ src/components/CommentCard.jsx
@@ -0,0 +1,42 @@
+import Avatar from "./Avatar.jsx";
+import { Link } from "react-router";
+
+function formatDate(date) {
+  return new Date(date).toLocaleString("en-US", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+}
+
+export default function CommentCard({ comment, currentUserPicture }) {
+  const commentCreatedAt = formatDate(comment.createdAt);
+
+  return (
+    <div className="comment-card">
+      <div className="comment-info-container">
+        <Avatar
+          picture={
+            comment.author.picture?.split("?")[0] ===
+            currentUserPicture?.split("?")[0]
+              ? currentUserPicture
+              : comment.author.picture
+          }
+        />
+
+        <div className="comment-author-time">
+          <Link
+            to={`/profile/${comment.author.id}`}
+            className="comment-author-name"
+          >
+            {comment.author.displayName}
+          </Link>
+          <p className="comment-create-time">{commentCreatedAt}</p>
+        </div>
+      </div>
+      <p className="comment-content">{comment.content}</p>
+    </div>
+  );
+}
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,16 +1,5 @@
 import { useRef, useState } from "react";
-import Avatar from "./Avatar.jsx";
-import { Link } from "react-router";
-
-function formatDate(date) {
-  return new Date(date).toLocaleString("en-US", {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-}
+import CommentCard from "./CommentCard.jsx";
 
 export default function Comments({
   currentUserPicture,
@@ -99,36 +88,6 @@ export default function Comments({
         </button>
       </form>
       {errors.content && <p className="field-error">{errors.content}</p>}
-    </div>
-  );
-}
-
-function CommentCard({ comment, currentUserPicture }) {
-  const commentCreatedAt = formatDate(comment.createdAt);
-
-  return (
-    <div className="comment-card">
-      <div className="comment-info-container">
-        <Avatar
-          picture={
-            comment.author.picture?.split("?")[0] ===
-            currentUserPicture?.split("?")[0]
-              ? currentUserPicture
-              : comment.author.picture
-          }
-        />
-
-        <div className="comment-author-time">
-          <Link
-            to={`/profile/${comment.author.id}`}
-            className="comment-author-name"
-          >
-            {comment.author.displayName}
-          </Link>
-          <p className="comment-create-time">{commentCreatedAt}</p>
-        </div>
-      </div>
-      <p className="comment-content">{comment.content}</p>
     </div>
   );
 }
