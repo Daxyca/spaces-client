4fe45b26af5425480adc3849689ed9cb55b4d6e8
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-10-10 22:37:59 +0800

    feat: confirm password input on registration

--- src/components/auth/RegisterForm.jsx
+++ src/components/auth/RegisterForm.jsx
@@ -7,6 +7,8 @@ export default function RegisterForm() {
   const navigate = useNavigate();
   const [errors, setErrors] = useState({});
   const pending = useRef();
+  const passwordInput = useRef();
+  const confirmPasswordInput = useRef();
 
   function handleRegisterSubmit(event) {
     event.preventDefault();
@@ -16,13 +18,20 @@ export default function RegisterForm() {
     const username = formData.get("username");
     const email = formData.get("email");
     const password = formData.get("password");
+    const confirmPassword = formData.get("confirmPassword");
+    setErrors({});
+    if (password !== confirmPassword) {
+      setErrors({ confirmPassword: "Passwords do not match." });
+      pending.current = false;
+      return;
+    }
     const register = async () => {
       try {
         const endpoint = import.meta.env.VITE_API_URL + "/auth/register";
         const res = await fetch(endpoint, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ username, email, password }),
+          body: JSON.stringify({ username, email, password, confirmPassword }),
         });
         const json = await res.json();
         if (!json.error) {
@@ -39,6 +48,20 @@ export default function RegisterForm() {
     };
     register();
   }
+
+  const handlePasswordInput = (event) => {
+    const password = passwordInput.current.value;
+    const confirmPassword = confirmPasswordInput.current.value;
+    if (confirmPassword === "" || password !== confirmPassword) {
+      confirmPasswordInput.current.style.outline = "0.0625rem solid red";
+      confirmPasswordInput.current.setCustomValidity(
+        "Must be same as password."
+      );
+    } else {
+      confirmPasswordInput.current.style.outline = "none";
+      confirmPasswordInput.current.setCustomValidity("");
+    }
+  };
 
   return (
     <>
@@ -77,9 +100,28 @@ export default function RegisterForm() {
           minLength="3"
           maxLength="32"
           placeholder="Password"
+          onInput={handlePasswordInput}
+          ref={passwordInput}
           required
         />
         {errors.password && <p className="field-error">{errors.password}</p>}
+        <label className="sr-only" htmlFor="confirmPassword">
+          Confirm Password:
+        </label>
+        <input
+          type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+          minLength="3"
+          maxLength="32"
+          placeholder="Confirm Password"
+          onInput={handlePasswordInput}
+          ref={confirmPasswordInput}
+          required
+        />
+        {errors.confirmPassword && (
+          <p className="field-error">{errors.confirmPassword}</p>
+        )}
         <button className="button" type="submit">
           Register
         </button>
