6e2adda1fcac7db1563d7941282eb3ca280e50af
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-10-10 23:47:09 +0800

    feat: add placeholder loader for profile

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -2,6 +2,7 @@ import { useAuth } from "../contexts/AuthContext.js";
 import { Outlet, useParams } from "react-router-dom";
 import "../styles/ProfilePage.css";
 import { useProfile } from "../contexts/ProfileContext.js";
+import { Loading } from "../components/Loading.jsx";
 
 export default function ProfilePage() {
   const { userId } = useParams();
@@ -9,7 +10,7 @@ export default function ProfilePage() {
   const { profile, setProfile } = useProfile();
 
   if (!profile || Object.keys(profile).length === 0 || !user?.id) {
-    return;
+    return <Loading />;
   }
 
   const isCurrentUser = !userId || userId === user?.id;
