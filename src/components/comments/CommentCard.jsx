a2bdf97ef97e16df720d892d1038cb8292ea6575
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-10-10 10:46:46 +0800

    feat: user can now edit own comment

--- src/components/comments/CommentCard.jsx
+++ src/components/comments/CommentCard.jsx
@@ -22,6 +22,8 @@ export default function CommentCard({
   const [menuShown, setMenuShown] = useState(false);
   const pending = useRef();
   const [isEditing, setIsEditing] = useState(false);
+  const editCommentForm = useRef();
+  const [errors, setErrors] = useState({});
 
   const commentCreatedAt = formatDate(comment.createdAt);
 
@@ -69,6 +71,63 @@ export default function CommentCard({
   const handleEditCommentClick = () => {
     setMenuShown(false);
     setIsEditing(true);
+  };
+
+  const handleCommentInputKeyDown = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      editCommentForm.current.requestSubmit();
+    } else if (event.key === "Escape") {
+      setIsEditing(false);
+    }
+  };
+
+  const handleCancelEditClick = (event) => {
+    event.preventDefault();
+    setIsEditing(false);
+  };
+
+  const handleEditCommentFormSubmit = (event) => {
+    event.preventDefault();
+    if (pending.current) return;
+    pending.current = true;
+    const form = event.target;
+    const formData = new FormData(form);
+    const content = formData.get("content").trim();
+    const editComment = async () => {
+      try {
+        const endpoint =
+          import.meta.env.VITE_API_URL + "/comments/" + comment.id;
+        const res = await fetch(endpoint, {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+          body: JSON.stringify({ content }),
+        });
+        const json = await res.json();
+        if (!json.error) {
+          setErrors({});
+          setComments((prevComments) =>
+            prevComments.map((prevComment) =>
+              prevComment.id === json.id ? json : prevComment
+            )
+          );
+          setIsEditing(false);
+        } else {
+          setErrors(parseValidationErrors(res.status, json));
+        }
+      } catch (err) {
+        console.error(err);
+      } finally {
+        pending.current = false;
+      }
+    };
+    if (content) {
+      editComment();
+    } else {
+      pending.current = false;
+      setErrors({ content: "Comment must not be empty or whitespaces only." });
+    }
   };
 
   return (
@@ -123,7 +182,43 @@ export default function CommentCard({
           </div>
         ) : null}
       </div>
-      <p className="comment-content">{comment.content}</p>
+      {isEditing ? (
+        <form
+          className="edit-comment-form"
+          ref={editCommentForm}
+          onSubmit={handleEditCommentFormSubmit}
+        >
+          <label htmlFor={comment.id + "-content"} className="sr-only">
+            Comment content:
+          </label>
+          <input
+            name="content"
+            id={comment.id + "-content"}
+            className="edit-comment-content-input"
+            defaultValue={comment.content}
+            onKeyDown={handleCommentInputKeyDown}
+            autoFocus
+          ></input>
+          <div className="edit-comment-buttons-container">
+            <span className="shortcut-key">Ctrl + ↵</span>
+            <button className="button" type="submit" name="save">
+              Save Edit
+            </button>
+            <span className="shortcut-key">Esc</span>
+            <button
+              className="button alt"
+              type="submit"
+              name="cancel"
+              onClick={handleCancelEditClick}
+            >
+              Cancel
+            </button>
+          </div>
+          {errors.content && <p className="field-error">{errors.content}</p>}
+        </form>
+      ) : (
+        <p className="comment-content">{comment.content}</p>
+      )}
     </div>
   );
 }
