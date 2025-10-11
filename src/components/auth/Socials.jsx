4e0f526512151b9665a9457f166d326a3389f68f
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-10-11 18:47:19 +0800

    fix: use link for github auth

--- src/components/auth/Socials.jsx
+++ src/components/auth/Socials.jsx
@@ -1,23 +1,14 @@
 export default function Socials({ loginForm = true }) {
-  const handleGithubFormSubmit = (event) => {
-    event.preventDefault();
-    localStorage.setItem("login", "true");
-    event.target.submit();
-  };
-
   return (
     <>
       <hr />
-      <form
-        className="auth-form github-form"
-        action={import.meta.env.VITE_API_URL + "/auth/github"}
-        onSubmit={handleGithubFormSubmit}
-        method="get"
+      <a
+        className="github-auth-link"
+        href={import.meta.env.VITE_API_URL + "/auth/github"}
+        onClick={() => localStorage.setItem("login", "true")}
       >
-        <button type="submit">
-          {loginForm ? "Login" : "Register"} with GitHub
-        </button>
-      </form>
+        {loginForm ? "Login" : "Register"} with GitHub
+      </a>
     </>
   );
 }
