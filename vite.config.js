4c4fef467b170764e70102f3b7a6a9ad515a45a5
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-10-12 21:19:05 +0800

    build: remove basicSsl

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import process from "process";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import "dotenv/config";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   base: process.env.VITE_BASENAME,
-  preview: {
-    host: "frontend.local",
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: process.env.VITE_SERVER_URL,
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  server: {
-    host: "frontend.local",
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: process.env.VITE_SERVER_URL,
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
